44ea6c4d3b3e2c79dbb6787be327bc2357397123
Андрей Калашников <a.kalashnikov@burda.ru>
2026-08-09 18:49:26 +0300

    7.2. Подключим redux-thunk.

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,4 +1,15 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { reducer } from './reducer';
+import { createAPI } from '../servieces/api';
 
-export const store = configureStore({reducer});
+const api = createAPI();
+
+export const store = configureStore({
+  reducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      thunk: {
+        extraArgument: api,
+      },
+    }),
+});
